923d750e62e6dcbf144c3455f163b63cbb8a5728
Juliette Sinibardy <juju.siny@gmail.com>
2020-05-12 15:37:49 +0200

    :white_check_mark: Write unit test for trickPlayerOrder

--- src/tests/actions.test.ts
+++ src/tests/actions.test.ts
@@ -1,6 +1,9 @@
-import { MapSchema } from '@colyseus/schema';
-import { findHighestCard, createDeck } from '../utils';
+import { MapSchema, ArraySchema } from '@colyseus/schema';
+import { findHighestCard, createDeck, computeTrickPlayerOrder } from '../utils';
 import { Card } from '../Card';
+import { Round } from '../Round';
+import { PlayerHand } from '../PlayerHand';
+import { PlayerRoundScore } from '../PlayerRoundScore';
 
 test('The highest blue card is at index 3 (12 Blue)', () => {
   const suit = 'blue';
@@ -14,4 +17,31 @@ test('The highest blue card is at index 3 (12 Blue)', () => {
   }
 
   expect(findHighestCard(suit, cardsPlayed)).toBe(3);
+});
+
+test('Compute trick player order at various stages of a round', () => {
+  let absolutePlayerOrder = new ArraySchema<number>();
+  [3, 4, 2, 1, 5].forEach((id) => absolutePlayerOrder.push(id));
+  const round = new Round(
+    3,
+    4, // first trick player
+    new MapSchema<PlayerHand>(),
+    new MapSchema<PlayerRoundScore>()
+  );
+
+  // first trick of the round
+  let trickPlayerOrder1 = new ArraySchema<number>();
+  [4, 2, 1, 5, 3].forEach((id) => trickPlayerOrder1.push(id));
+
+  expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
+    trickPlayerOrder1
+  );
+
+  // new trick (still the same round)
+  round.firstPlayer = 5;
+  let trickPlayerOrder2 = new ArraySchema<number>();
+  [5, 3, 4, 2, 1].forEach((id) => trickPlayerOrder2.push(id));
+  expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
+    trickPlayerOrder2
+  );
 });
