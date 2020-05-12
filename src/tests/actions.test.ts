4c27898a27033e71fe1d9f54200b509c5bf0088a
Juliette Sinibardy <juju.siny@gmail.com>
2020-05-12 15:45:57 +0200

    :recycle: Refactor test suite

--- src/tests/actions.test.ts
+++ src/tests/actions.test.ts
@@ -5,8 +5,7 @@ import { Round } from '../Round';
 import { PlayerHand } from '../PlayerHand';
 import { PlayerRoundScore } from '../PlayerRoundScore';
 
-test('The highest blue card is at index 3 (12 Blue)', () => {
-  const suit = 'blue';
+describe('findHighestCard()', () => {
   const deck = createDeck();
   const cards = [deck[3], deck[13], deck[15], deck[24], deck[41], deck[65]];
   // 4 rouge, 1 bleu, 3 bleu, 12 bleu, 3 noir, bloody Mary
@@ -16,12 +15,16 @@ test('The highest blue card is at index 3 (12 Blue)', () => {
     cardsPlayed[i] = cards[i];
   }
 
-  expect(findHighestCard(suit, cardsPlayed)).toBe(3);
+  test('The highest blue card is at index 3 (12 Blue)', () => {
+    const suit = 'blue';
+    expect(findHighestCard(suit, cardsPlayed)).toBe(3);
+  });
 });
 
-test('Compute trick player order at various stages of a round', () => {
+describe('computeTrickPlayerOrder()', () => {
   let absolutePlayerOrder = new ArraySchema<number>();
   [3, 4, 2, 1, 5].forEach((id) => absolutePlayerOrder.push(id));
+
   const round = new Round(
     3,
     4, // first trick player
@@ -29,19 +32,22 @@ test('Compute trick player order at various stages of a round', () => {
     new MapSchema<PlayerRoundScore>()
   );
 
-  // first trick of the round
-  let trickPlayerOrder1 = new ArraySchema<number>();
-  [4, 2, 1, 5, 3].forEach((id) => trickPlayerOrder1.push(id));
+  test('At first trick of a round', () => {
+    let trickPlayerOrder1 = new ArraySchema<number>();
+    [4, 2, 1, 5, 3].forEach((id) => trickPlayerOrder1.push(id));
 
-  expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
-    trickPlayerOrder1
-  );
+    expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
+      trickPlayerOrder1
+    );
+  });
 
-  // new trick (still the same round)
-  round.firstPlayer = 5;
-  let trickPlayerOrder2 = new ArraySchema<number>();
-  [5, 3, 4, 2, 1].forEach((id) => trickPlayerOrder2.push(id));
-  expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
-    trickPlayerOrder2
-  );
+  test('During the round', () => {
+    round.firstPlayer = 5;
+    let trickPlayerOrder2 = new ArraySchema<number>();
+    [5, 3, 4, 2, 1].forEach((id) => trickPlayerOrder2.push(id));
+
+    expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
+      trickPlayerOrder2
+    );
+  });
 });
