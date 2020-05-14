ce29b6b00ee0f8dc7d37f369e69a50e960782450
Juliette Sinibardy <juju.siny@gmail.com>
2020-05-14 16:34:46 +0200

    :rotating_light: Clean up most of test file

--- src/tests/actions.test.ts
+++ src/tests/actions.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-sparse-arrays */
+
 import { MapSchema, ArraySchema } from '@colyseus/schema';
 import {
   findHighestCardOwner,
@@ -16,7 +18,7 @@ describe('findHighestCardOwner()', () => {
 
   const trick = setupTrick([cardsId, 'blue', , 'escape']);
 
-  test('The highest blue card is player 2 (12 Blue)', () => {
+  it('The highest blue card is player 2 (12 Blue)', () => {
     expect(findHighestCardOwner(trick.suit, trick.cardsPlayed)).toBe(2);
   });
 });
@@ -32,20 +34,18 @@ describe('computeTrickPlayerOrder()', () => {
     new MapSchema<PlayerRoundScore>()
   );
 
-  test('At first trick of a round', () => {
-    let trickPlayerOrder1 = new ArraySchema<number>();
+  it('At first trick of a round', () => {
+    const trickPlayerOrder1 = new ArraySchema<number>();
     [4, 2, 1, 5, 3].forEach((id) => trickPlayerOrder1.push(id));
-
     expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
       trickPlayerOrder1
     );
   });
 
-  test('During the round', () => {
+  it('During the round', () => {
     round.firstPlayer = 5;
-    let trickPlayerOrder2 = new ArraySchema<number>();
+    const trickPlayerOrder2 = new ArraySchema<number>();
     [5, 3, 4, 2, 1].forEach((id) => trickPlayerOrder2.push(id));
-
     expect(computeTrickPlayerOrder(round, absolutePlayerOrder)).toEqual(
       trickPlayerOrder2
     );
@@ -53,7 +53,7 @@ describe('computeTrickPlayerOrder()', () => {
 });
 
 describe('findFirstCardOwner()', () => {
-  test('Find first mermaid played', () => {
+  it('Find first mermaid played', () => {
     const cardsId = [53, 13, 54, 24, 41, 65];
     // sirène, 1 bleu, sirène, 12 bleu, 3 noir, bloody Mary
 
@@ -64,7 +64,7 @@ describe('findFirstCardOwner()', () => {
     ).toBe(5);
   });
 
-  test('Find first pirate played, with Bloody Mary', () => {
+  it('Find first pirate played, with Bloody Mary', () => {
     const cardsId = [53, 58, 54, 57, 41, 65];
     // sirène, pirate, sirène, pirate, 3 noir, bloody Mary
 
@@ -80,7 +80,7 @@ describe('findFirstCardOwner()', () => {
     ).toBe(3);
   });
 
-  test('Find first pirate played, with Bloody Mary as a winner', () => {
+  it('Find first pirate played, with Bloody Mary as a winner', () => {
     const cardsId = [65, 58, 54, 57, 41, 58];
     // bloody Mary, pirate, sirène, pirate, 3 noir, pirate
 
@@ -98,14 +98,14 @@ describe('findFirstCardOwner()', () => {
 });
 
 describe('computeWinner()', () => {
-  test('Black wins over suit cards (Mary as an Escape)', () => {
+  it('Black wins over suit cards (Mary as an Escape)', () => {
     const trick = setupTrick();
     const result = { winner: 1, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Pirates win over Black cards and Mermaids', () => {
+  it('Pirates win over Black cards and Mermaids', () => {
     const cardsId = [53, 58, 54, 57, 41, 65];
     // sirène, pirate, sirène, pirate, 3 noir, bloody Mary
 
@@ -113,10 +113,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 3, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Mermaids win over black cards', () => {
+  it('Mermaids win over black cards', () => {
     const cardsId = [51, 16, 53, 54, 41, 64];
     //  13 noir, 4 bleu, sirène, sirène, 3 noir, escape
 
@@ -124,10 +124,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 4, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Pirates win over mermaids', () => {
+  it('Pirates win over mermaids', () => {
     const cardsId = [51, 16, 53, 58, 41, 64];
     //  13 noir, 4 bleu, sirène, pirate, 3 noir, escape
 
@@ -135,10 +135,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 2, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Mermaids win over the Skull King (and SK counter is incremented)', () => {
+  it('Mermaids win over the Skull King (and SK counter is incremented)', () => {
     const cardsId = [52, 16, 53, 51, 41, 64];
     //  skull king, 4 bleu, sirène, 13 noir, 3 noir, escape
 
@@ -146,10 +146,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 4, skullKingCaptured: 1, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Mermaids win over the Skull King even with pirates', () => {
+  it('Mermaids win over the Skull King even with pirates', () => {
     const cardsId = [52, 16, 53, 58, 41, 54];
     //  skull king, 4 bleu, sirène, pirate, 3 noir, sirène
 
@@ -157,10 +157,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 4, skullKingCaptured: 1, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Skull King captures pirates', () => {
+  it('Skull King captures pirates', () => {
     const cardsId = [52, 16, 58, 51, 41, 56];
     //  skull king, 4 bleu, pirate, 13 noir, 3 noir, pirate
 
@@ -168,10 +168,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 5, skullKingCaptured: 0, piratesCaptured: 2 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Skull King captures bloody Mary (escape)', () => {
+  it('Skull King captures bloody Mary (escape)', () => {
     const cardsId = [52, 16, 65, 51, 41, 13];
     //  skull king, 4 bleu, bloody Mary, 13 noir, 3 noir, 1 bleu
 
@@ -179,10 +179,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 5, skullKingCaptured: 0, piratesCaptured: 1 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Skull King captures bloody Mary (pirate)', () => {
+  it('Skull King captures bloody Mary (pirate)', () => {
     const cardsId = [52, 16, 65, 51, 41, 13];
     //  skull king, 4 bleu, bloody Mary, 13 noir, 3 noir, 1 bleu
 
@@ -190,10 +190,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 5, skullKingCaptured: 0, piratesCaptured: 1 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Flags only: winner is the first player', () => {
+  it('Flags only: winner is the first player', () => {
     const cardsId = [63, 61, 60, 62, 64];
     // all escape flags
     const playerOrder = [4, 2, 1, 3, 5];
@@ -202,10 +202,10 @@ describe('computeWinner()', () => {
 
     const result = { winner: 4, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 
-  test('Suit color wins over higher cards of other suits (except black)', () => {
+  it('Suit color wins over higher cards of other suits (except black)', () => {
     const cardsId = [13, 12, 38, 11, 35, 37];
     // 1 bleu, 13 rouge, 13 jaune, 12 rouge, 10 jaune, 12 jaune
 
@@ -213,6 +213,6 @@ describe('computeWinner()', () => {
 
     const result = { winner: 5, skullKingCaptured: 0, piratesCaptured: 0 };
 
-    expect(computeWinner(trick)).toEqual(result);
+    expect(computeWinner(trick)).toStrictEqual(result);
   });
 });
